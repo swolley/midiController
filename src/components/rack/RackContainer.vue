1bf6b855a7c37a4f56ae15a0b3188cd146eb856e
swolley <myentity@inwind.it>
2023-05-10 23:38:31 +0200

    refactor(rack container): moved slot components from HomeView directly into RackContainer

--- src/components/rack/RackContainer.vue
+++ src/components/rack/RackContainer.vue
@@ -1,47 +1,187 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import ToolBar from "@/components/toolbar/ToolBar.vue";
+import { useRack } from "@/stores/useRack";
+import OutboardsContainer from "@/components/rack/OutboardsContainer.vue";
+import ConsoleDevice from "@/components/devices/ConsoleDevice.vue";
+import OutboardDevice from "@/components/devices/OutboardDevice.vue";
+import type { Midi } from "@/services/classes/Midi";
+import { Container, Draggable } from "vue-dndrop";
+import type RackConsole from "@/services/classes/RackConsole";
+import type { Outboard } from "@/services/classes/Outboard";
+import { useWindowSize } from "@vueuse/core";
+import type { DropResult } from "@/services/types/devices";
+import { ref, computed, watch } from "vue";
+
+defineEmits<{
+    (event: "openeditor", device: Outboard): void;
+    (event: "togglestore"): void;
+    (event: "togglesettings"): void;
+}>();
+
+const rackStore = useRack();
+
+const collapseAll = ref<boolean>(false);
+const selectedDevice = ref<Outboard | undefined>(rackStore.rackDevices.length ? (rackStore.rackDevices[0] as Outboard) : undefined);
+const showConsole = ref<boolean>(true);
+
+const { width } = useWindowSize();
+const isDesktop = computed(() => width.value >= 1024);
+
+function toggleCollapseAll(collapse: boolean) {
+    collapseAll.value = collapse;
+}
+
+function onDrop(dropResult: DropResult) {
+    if (dropResult.removedIndex === null && dropResult.addedIndex !== null) {
+        rackStore.moveDeviceToRack(dropResult.payload.device, dropResult.addedIndex);
+        selectedDevice.value = dropResult.payload.device;
+    } else if (dropResult.removedIndex !== null && dropResult.addedIndex !== null && dropResult.payload.list === "rack") {
+        rackStore.reorderRackDevices(dropResult.payload.device, dropResult.removedIndex, dropResult.addedIndex);
+    }
+}
+
+function focusDevice(device: Outboard) {
+    if (selectedDevice.value !== device) selectedDevice.value = device;
+}
+
+function moveBackToStore(device: Outboard) {
+    rackStore.moveDeviceBackToStore(device);
+}
+
+watch(
+    () => selectedDevice.value,
+    () => {
+        if (selectedDevice.value) document.title = "App - " + selectedDevice.value.label;
+    }
+);
+</script>
 
 <template>
-    <div class="rack relative flex flex-col">
-        <div class="rack-side left"></div>
-        <div class="rack-side right"></div>
-        <!-- preamps -->
-        <div class="rack-devices grow overflow-y-scroll">
-            <slot></slot>
+    <div class="h-full table">
+        <div class="table-row">
+            <div class="hidden md:table-cell cell lt"></div>
+            <div class="table-cell cell ct">
+                <!-- header -->
+                <ToolBar
+                    class="mx-2 my-3"
+                    :show-toggle-console="isDesktop"
+                    @toggleconsole="showConsole = !showConsole"
+                    @togglestore="$emit('togglestore')"
+                    @togglecollapseall="toggleCollapseAll"
+                    @togglesettings="$emit('togglesettings')"
+                />
+            </div>
+            <div class="hidden md:table-cell cell rt"></div>
         </div>
-        <!-- footer -->
-        <div class="relative z-10">
-            <slot name="footer"></slot>
+
+        <div class="table-row">
+            <div class="hidden md:table-cell cell lc"></div>
+            <div class="table-cell cell cc">
+                <!-- box -->
+                <OutboardsContainer :showConsole="showConsole">
+                    <Container
+                        class="h-full overflow-y-scroll"
+                        :get-child-payload="(index: number) => ({ list: 'rack', device: rackStore.rackDevices[index] })"
+                        :fire-related-events-only="true"
+                        drag-handle-selector=".column-drag-handle"
+                        group-name="devices"
+                        data-index="rack"
+                        orientation="vertical"
+                        @drop="(drop: DropResult) => onDrop(drop)"
+                        v-if="rackStore.midi !== undefined || rackStore.http !== undefined"
+                    >
+                        <!-- devices -->
+                        <Draggable v-for="device in rackStore.rackDevices" :key="device.id">
+                            <OutboardDevice
+                                :background="device.backgroundColor"
+                                :collapsable="true"
+                                :draggable="true"
+                                :collapsed="collapseAll"
+                                :selected="selectedDevice !== undefined && selectedDevice.id === device.id"
+                                :device="(device as Outboard)"
+                                :midi="(rackStore.midi as Midi)"
+                                :is-selected="selectedDevice !== undefined && selectedDevice.id === device.id"
+                                @openeditor="() => $emit('openeditor', device)"
+                                @removedevice="() => moveBackToStore(device as Outboard)"
+                                @click="focusDevice(device as Outboard)"
+                            />
+                        </Draggable>
+                    </Container>
+
+                    <template v-slot:footer v-if="rackStore.console && isDesktop">
+                        <!-- console -->
+                        <ConsoleDevice
+                            v-show="showConsole && rackStore.console"
+                            :console="(rackStore.console as RackConsole)"
+                            :collapsable="true"
+                            :collapsed="collapseAll"
+                        />
+                    </template>
+                </OutboardsContainer>
+            </div>
+            <div class="hidden md:table-cell cell rc"></div>
+        </div>
+
+        <div class="table-row">
+            <div class="hidden md:table-cell cell lb"></div>
+            <div class="table-cell cell cb"></div>
+            <div class="hidden md:table-cell cell rb"></div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-.rack {
-    margin: -6px 0px 0px 3px;
-    height: calc(100% + 12px);
-
-    .rack-side {
-        @apply absolute h-full top-0 bottom-0 z-0;
-        width: 50px;
-        background-repeat: repeat-y;
-
-        &.left {
-            @apply left-0;
-            background-image: url(/interno_sx.gif);
-        }
-        &.right {
-            @apply right-0;
-            background-image: url(/interno_dx.gif);
+.table-row {
+    &:first-child,
+    &:last-child {
+        .cell {
+            height: 61px;
         }
     }
 
-    .rack-devices {
-        @apply z-10 relative;
-        // height: calc(100% - 110px);
-        overflow-y: scroll;
+    &:nth-child(2) {
+        .cell:first-child,
+        .cell:last-child {
+            background-repeat: repeat-y;
+        }
     }
-    // .rack .rack-devices:has(+ .preamp-display:not(.full)) {
-    //     height: calc(100% - 66px);
-    // }
+
+    .cell {
+        &:first-child,
+        &:last-child {
+            background-repeat: no-repeat;
+            width: 53px;
+        }
+
+        &:nth-child(2) {
+            background-repeat: repeat-x;
+        }
+
+        &.lt {
+            background-image: url(/box-top-left.jpg);
+        }
+        &.ct {
+            background-image: url(/box-top-center.jpg);
+        }
+        &.rt {
+            background-image: url(/box-top-right.jpg);
+        }
+        &.lc {
+            background-image: url(/box-center-left.jpg);
+        }
+        &.rc {
+            background-image: url(/box-center-right.jpg);
+        }
+        &.lb {
+            background-image: url(/box-bottom-left.jpg);
+        }
+        &.cb {
+            background-image: url(/box-bottom-center.jpg);
+            background-position-y: bottom;
+        }
+        &.rb {
+            background-image: url(/box-bottom-right.jpg);
+        }
+    }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,65 +1,36 @@
 <script setup lang="ts">
-import BoxContainer from "@/components/rack/BoxContainer.vue";
 import RackContainer from "@/components/rack/RackContainer.vue";
-import ConsoleDevice from "@/components/devices/ConsoleDevice.vue";
-import ToolBar from "@/components/toolbar/ToolBar.vue";
-import { computed, ref, watch } from "vue";
+import { ref } from "vue";
 import { useRack } from "@/stores/useRack";
 import ModalPanel from "@/components/modals/ModalPanel.vue";
-import OutboardDevice from "@/components/devices/OutboardDevice.vue";
-import type { Midi } from "@/services/classes/Midi";
-import type { DropResult } from "@/services/types/devices";
-import { Container, Draggable } from "vue-dndrop";
 import SidebarStore from "@/components/devices/SidebarStore.vue";
 import DeviceEditor from "@/components/editor/DeviceEditor.vue";
-import type RackConsole from "@/services/classes/RackConsole";
 import AppConfig from "@/components/editor/AppConfig.vue";
-import type { Outboard } from "@/services/classes/Outboard";
-import { useWindowSize } from "@vueuse/core";
+import { Outboard } from "@/services/classes/Outboard";
 
 const rackStore = useRack();
-const { width } = useWindowSize();
-const isDesktop = computed(() => width.value >= 1024);
 
-const showConsole = ref<boolean>(true);
-const collapseAll = ref<boolean>(false);
 const showStore = ref<boolean>(rackStore.rackDevices.length === 0);
-const selectedDevice = ref<Outboard | undefined>(rackStore.rackDevices.length ? (rackStore.rackDevices[0] as Outboard) : undefined);
 const modifiedDevice = ref<Outboard | undefined>();
 const openSettings = ref<boolean>(false);
 const openEditor = ref<boolean>(false);
 
 function cloneDevice(device: Outboard) {
-    return JSON.parse(JSON.stringify(device)) as Outboard;
+    return new Outboard(JSON.parse(JSON.stringify(device))) as Outboard;
 }
 
-function toggleEditor(device?: Outboard) {
+function toggleEditor(device?: Outboard, askConfirm = false) {
     if (device) {
-        selectedDevice.value = device;
         modifiedDevice.value = cloneDevice(device);
-    } else {
-        modifiedDevice.value = device;
+    } else if (askConfirm && confirm("Do you want to cancel this device edting?")) {
+        modifiedDevice.value = undefined;
     }
     openEditor.value = device !== undefined;
 }
 function saveAndCloseEditor() {
     if (modifiedDevice.value) {
-        selectedDevice.value = cloneDevice(modifiedDevice.value);
         modifiedDevice.value = undefined;
         openEditor.value = false;
-    }
-}
-
-function toggleCollapseAll(collapse: boolean) {
-    collapseAll.value = collapse;
-}
-
-function onDrop(dropResult: DropResult) {
-    if (dropResult.removedIndex === null && dropResult.addedIndex !== null) {
-        rackStore.moveDeviceToRack(dropResult.payload.device, dropResult.addedIndex);
-        selectedDevice.value = dropResult.payload.device;
-    } else if (dropResult.removedIndex !== null && dropResult.addedIndex !== null && dropResult.payload.list === "rack") {
-        rackStore.reorderRackDevices(dropResult.payload.device, dropResult.removedIndex, dropResult.addedIndex);
     }
 }
 
@@ -84,81 +55,19 @@ function createDevice() {
     const newDevice = rackStore.createNewDevice();
     toggleEditor(newDevice);
 }
-
-function onDeviceClick(device: Outboard) {
-    selectedDevice.value = device;
-}
-
-function moveBackToStore(device: Outboard) {
-    rackStore.moveDeviceBackToStore(device);
-}
-
-watch(
-    () => selectedDevice.value,
-    () => {
-        if (selectedDevice.value) document.title = "App - " + selectedDevice.value.label;
-    }
-);
 </script>
 
 <template>
     <main class="h-full flex">
         <SidebarStore :show="showStore" @openeditor="toggleEditor" @createdevice="createDevice" />
-        <!-- box -->
-        <BoxContainer class="w-full" @touchstart="touchStart">
-            <template v-slot:header>
-                <!-- header -->
-                <ToolBar
-                    class="mx-2 my-3"
-                    :show-toggle-console="isDesktop"
-                    @toggleconsole="showConsole = !showConsole"
-                    @togglestore="showStore = !showStore"
-                    @togglecollapseall="toggleCollapseAll"
-                    @togglesettings="openSettings = !openSettings"
-                />
-            </template>
-            <!-- rack -->
-            <RackContainer :showConsole="showConsole">
-                <Container
-                    class="h-full"
-                    :get-child-payload="(index: number) => ({ list: 'rack', device: rackStore.rackDevices[index] })"
-                    :fire-related-events-only="true"
-                    drag-handle-selector=".column-drag-handle"
-                    group-name="devices"
-                    data-index="rack"
-                    orientation="vertical"
-                    @drop="(drop: DropResult) => onDrop(drop)"
-                    v-if="rackStore.midi !== undefined || rackStore.http !== undefined"
-                >
-                    <!-- devices -->
-                    <Draggable v-for="device in rackStore.rackDevices" :key="device.id">
-                        <OutboardDevice
-                            :background="device.backgroundColor"
-                            :collapsable="true"
-                            :draggable="true"
-                            :collapsed="collapseAll"
-                            :selected="selectedDevice !== undefined && selectedDevice.id === device.id"
-                            :device="(device as Outboard)"
-                            :midi="(rackStore.midi as Midi)"
-                            :is-selected="selectedDevice !== undefined && selectedDevice.id === device.id"
-                            @openeditor="() => toggleEditor(device as Outboard)"
-                            @removedevice="() => moveBackToStore(device as Outboard)"
-                            @click="onDeviceClick(device as Outboard)"
-                        />
-                    </Draggable>
-                </Container>
 
-                <template v-slot:footer v-if="rackStore.console && isDesktop">
-                    <!-- console -->
-                    <ConsoleDevice
-                        v-show="showConsole && rackStore.console"
-                        :console="(rackStore.console as RackConsole)"
-                        :collapsable="true"
-                        :collapsed="collapseAll"
-                    />
-                </template>
-            </RackContainer>
-        </BoxContainer>
+        <RackContainer
+            class="w-full"
+            @touchstart="touchStart"
+            @openeditor="toggleEditor"
+            @togglestore="showStore = !showStore"
+            @togglesettings="openSettings = !openSettings"
+        />
 
         <Teleport to="body">
             <ModalPanel v-if="modifiedDevice && openEditor" :show="modifiedDevice && openEditor">
@@ -169,7 +78,7 @@ watch(
                     <div class="flex flex-col md:flex-row grow gap-2">
                         <button
                             class="rounded border border-white/10 w-full p-2 shadow bg-white/10 hover:bg-white/20 text-white transition-colors"
-                            @click="toggleEditor()"
+                            @click="toggleEditor(undefined, true)"
                         >
                             Cancel
                         </button>
